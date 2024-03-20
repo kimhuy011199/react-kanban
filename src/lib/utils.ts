e79e4fd523d85609cd6173c589350b6431e84e90
Nguyen Kim Huy <kimhuy011199@gmail.com>
2024-03-20 11:34:49 +0700

    Move format date function to utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,18 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
 
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
-}
+export const cn = (...inputs: ClassValue[]) => {
+  return twMerge(clsx(inputs));
+};
+
+export const formatCalendarDate = (date: Date | string) => {
+  const options: any = {
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+  };
+
+  return new Intl.DateTimeFormat('en-US', options).format(
+    typeof date === 'string' ? new Date(date) : date
+  );
+};
