f48c7acdc1899cece8e93f490fa1f31cb2d38452
Nguyen Kim Huy <kimhuy011199@gmail.com>
2024-03-21 20:57:17 +0700

    Init table view

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,5 +1,6 @@
 import { APP_VIEW } from '@/lib/enums';
 import Board from './Board';
+import Table from './Table';
 
 interface MainProps {
   currentView: APP_VIEW;
@@ -9,7 +10,7 @@ const Main = ({ currentView }: MainProps) => {
   return (
     <div className="px-7 py-5 bg-muted overflow-scroll board-area">
       {currentView === APP_VIEW.BOARD ? <Board /> : null}
-      {currentView === APP_VIEW.TABLE ? <>Table</> : null}
+      {currentView === APP_VIEW.TABLE ? <Table /> : null}
     </div>
   );
 };
---
+++ src/components/Table.tsx
@@ -0,0 +1,78 @@
+import { useSelector } from 'react-redux';
+import {
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+} from '@tanstack/react-table';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table';
+import { TABLE_COLUMNS } from '../lib/constants';
+import { RootState } from '@/store';
+
+const TableView = () => {
+  const data = useSelector((state: RootState) => state.todo.data);
+
+  const table = useReactTable({
+    data: data[0].tasks,
+    columns: TABLE_COLUMNS,
+    getCoreRowModel: getCoreRowModel(),
+  });
+
+  return (
+    <div className="rounded-md border">
+      <Table>
+        <TableHeader>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <TableRow key={headerGroup.id}>
+              {headerGroup.headers.map((header) => {
+                return (
+                  <TableHead key={header.id}>
+                    {header.isPlaceholder
+                      ? null
+                      : flexRender(
+                          header.column.columnDef.header,
+                          header.getContext()
+                        )}
+                  </TableHead>
+                );
+              })}
+            </TableRow>
+          ))}
+        </TableHeader>
+        <TableBody>
+          {table.getRowModel().rows?.length ? (
+            table.getRowModel().rows.map((row) => (
+              <TableRow
+                key={row.id}
+                data-state={row.getIsSelected() && 'selected'}
+              >
+                {row.getVisibleCells().map((cell) => (
+                  <TableCell key={cell.id}>
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  </TableCell>
+                ))}
+              </TableRow>
+            ))
+          ) : (
+            <TableRow>
+              <TableCell
+                colSpan={TABLE_COLUMNS.length}
+                className="h-24 text-center"
+              >
+                No results.
+              </TableCell>
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
+    </div>
+  );
+};
+
+export default TableView;
